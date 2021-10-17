498b79d6bda7d96118bdb9e4e6841a6a354c9146
acharyabibash <acharabibash@outlook.com>
2021-10-17 15:06:03 +0545

    02 Basics :001

---
+++ projects/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+def project(request,pk):
+    return HttpResponse('SINGLE PROJECT' + ' '+str(pk))
